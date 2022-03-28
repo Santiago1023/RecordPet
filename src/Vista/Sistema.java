88ca9efee00c7e9a1fc4485aba28c6611f4c0104
Santiago Bedoya <santiago.bedoyad@udea.edu.co>
2022-03-28 18:23:50 -0500

    "video 19- eliminar productos
    le damos click derecho en la tabla, mouse clicked y reutilizamos el codigo de TableProveedorMouseClicked
    
    reutilizamos el deleteCliente de la clase ClienteDao en ProductosDao
    
    agregamos codigo en el boton de eliminar, y creamos el metodo de limpiarProductos
    "

--- src/Vista/Sistema.java
+++ src/Vista/Sistema.java
@@ -778,6 +778,11 @@ public class Sistema extends javax.swing.JFrame {
                 "ID", "CODIGO", "DESCRIPCION", "PROVEEDOR", "STOCK", "PRECIO"
             }
         ));
+        TableProducto.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                TableProductoMouseClicked(evt);
+            }
+        });
         jScrollPane4.setViewportView(TableProducto);
         if (TableProducto.getColumnModel().getColumnCount() > 0) {
             TableProducto.getColumnModel().getColumn(0).setPreferredWidth(20);
@@ -798,6 +803,11 @@ public class Sistema extends javax.swing.JFrame {
         btnEditarPro.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/Actualizar (2).png"))); // NOI18N
 
         btnEliminarPro.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/eliminar.png"))); // NOI18N
+        btnEliminarPro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEliminarProActionPerformed(evt);
+            }
+        });
 
         btnNuevoPro.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/nuevo.png"))); // NOI18N
 
@@ -1224,6 +1234,32 @@ public class Sistema extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_btnGuardarProActionPerformed
 
+    private void TableProductoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_TableProductoMouseClicked
+        // TODO add your handling code here:
+        int fila = TableProducto.rowAtPoint(evt.getPoint());
+        txtIdPro.setText(TableProducto.getValueAt(fila,0).toString());
+        txtCodigoPro.setText(TableProducto.getValueAt(fila,1).toString());
+        txtDesPro.setText(TableProducto.getValueAt(fila,2).toString());
+        cbxProveedorPro.setSelectedItem(TableProducto.getValueAt(fila,3).toString());
+        txtCantPro.setText(TableProducto.getValueAt(fila,4).toString());
+        txtPrecioPro.setText(TableProducto.getValueAt(fila,5).toString());
+    }//GEN-LAST:event_TableProductoMouseClicked
+
+    private void btnEliminarProActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEliminarProActionPerformed
+        // TODO add your handling code here:
+        if(!"".equals(txtIdPro.getText())){
+            int pregunta = JOptionPane.showConfirmDialog(null, "Esta seguro de eliminar");
+            if(pregunta == 0){
+                int id = Integer.parseInt(txtIdPro.getText());
+                prodao.EliminarProductos(id);
+                LimpiarTable();
+                LimpiarProductos();
+                ListarProductos();
+                
+            }
+        }
+    }//GEN-LAST:event_btnEliminarProActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -1386,5 +1422,14 @@ public class Sistema extends javax.swing.JFrame {
         txtTelefonoProveedor.setText("");
         txtDireccionProveedor.setText("");
         txtRazonProveedor.setText("");
+    }
+    
+    private void LimpiarProductos(){
+        txtIdPro.setText("");
+        txtCodigoPro.setText("");
+        cbxProveedorPro.setSelectedItem(null);
+        txtDesPro.setText("");
+        txtCantPro.setText("");
+        txtPrecioPro.setText("");
     }
 }
